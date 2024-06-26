2ecb26f467cc31419d5d38640d668a887d729b23
AhmedMuraat <95277929+AhmedMuraat@users.noreply.github.com>
2024-06-26 19:25:57 +0200

    123

--- Locust/locustfile.py
+++ Locust/locustfile.py
@@ -1,25 +1,23 @@
 from locust import HttpUser, task, between
 import random
 import string
-
-def random_string(length=10):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(length))
+import json
 
 class UserBehavior(HttpUser):
     wait_time = between(1, 5)
 
-    @task
-    def register_user(self):
-        self.client.post("/api/auth/register", json={
-            "email": f"{random_string(5)}@example.com",
-            "username": random_string(8),
-            "password": "password123",
-            "name": random_string(5),
-            "lastName": random_string(5),
-            "address": random_string(15),
-            "dateOfBirth": "1998-10-23"
+    def on_start(self):
+        # Authentication and retrieving the JWT token
+        response = self.client.post("/api/auth/login", json={
+            "email": "admin@gmail.com",
+            "password": "123"
         })
+        self.token = response.json()['token']
+
+    @task
+    def get_user(self):
+        user_id = 5160  # You can parameterize this to test different user IDs
+        self.client.get(f"/api/userinfo/{user_id}", headers={"Authorization": f"Bearer {self.token}"})
 
 class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
